4d17bc4f78ce9b91cd4b64924028d737a2859ee5
WUTA46 <tsegazeabwuta@gmail.com>
2023-07-12 10:05:10 -0700

    Base code

---
+++ src/components/Banner.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import banner1 from '../images/d186e616-cd26-482a-a7ac-5633af6eb857.jpeg'
+
+
+function Banner() {
+  return (
+    <div className=''> 
+        <img 
+        className='h-[300px] banner'
+        src={banner1} alt="love to learn" />
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Header.css'
+import './Hamburger'
+import logo from '../images/green nature  illustration logo template social media (1).png'
+function Header() {
+  return (
+    <header className='flex shadow-md  items-center justify-between sticky top-0 z-50'>
+<div className='p-2'>
+    <img 
+    className='relative  rounded-md h-28 cursor-pointer'
+     src={logo} /> 
+</div>
+
+<div className=''>
+<p className='text-xl font-serif font-bold text-center'>LEMLEM  ACADEMY</p>
+</div>
+
+<div className=''>
+    <ul className='font-medium nav-link font-serif'>
+        <li className='my-4 cursor-pointer nav-item '><a href="#">Management</a></li>
+        <li className='my-4 cursor-pointer nav-item '><a href="#"></a>Achievement</li>
+        <li className='my-4 cursor-pointer nav-item '><a href="#">Gallery</a></li>
+    </ul>
+</div>
+
+<div className='nav-bar'>
+    <span className="bar"></span>
+    <span className="bar"></span>
+    <span className="bar"></span>
+</div>
+    </header>
+  )
+}
+
+export default Header
