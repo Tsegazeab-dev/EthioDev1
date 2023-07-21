4e90ec82c86f79f786a473dd8b0a59baa322c59f
DestawBerie <102520194+DestawBerie@users.noreply.github.com>
2023-07-21 15:52:43 +0300

    media query

--- src/components/Homepage/HomePage.js
+++ src/components/Homepage/HomePage.js
@@ -44,32 +44,32 @@ function HomePage() {
                   <li>
                     <h2 className="Mission">VISION</h2>
                     <p className="pback">
-                      Our vision is to offer young children an advanced
-                      education related with scientific and technological
-                      developments which takes the nation`s demand and social
-                      and cultural aspect into account. We strive to produce
-                      confident and hardworking citizens.
+                      Our Vission is to offer young chldren an advanced
+                      education related with scintific and technological
+                      development which takes the nation's demand, social and
+                      cultural aspects acount. We strive to produce confident
+                      and hardworking citizens
                     </p>
                   </li>
                   <li>
                     <h2 className="Mission">MISSION</h2>
                     <p className="pback">
-                      To give quality education and guide students to be
-                      creative, competent and capable of problem solving. To
-                      provide the required knowledge to enable students pass
-                      successfully the national exams and prepare students enter
-                      the university Working to improve the system of the
-                      learning and teaching activity and relate it with modern
-                      technology Work to produce a democratic, independent and
-                      confident citizen
+                      To give quality education and fuide students to be
+                      creative, competent and capable of problem solving To
+                      provide the required knowlodge to enable students pass
+                      successfully the national exams prepaire studentsenter the
+                      university. Working to improve the systemof he learning
+                      and teaching activity and relate it with model technology
+                      Work to produce a democratic, independent and confident
+                      sitizen.
                     </p>
                   </li>
                   <li>
                     <h2 className="Objectve"> Objective</h2>
                     <p className="pback">
-                      Working to meet the demands of the government, society and
+                      Working to meet the demand of the government socity and
                       parents in order to build capable citizens, who keep their
-                      promises identify and recognize Ethiopia`s beauty, unity
+                      promises identify and recognize Ethiopia's beauty, unity
                       and history.
                     </p>
                   </li>
--- src/components/Homepage/Mycomponent.js
+++ src/components/Homepage/Mycomponent.js
@@ -1,13 +1,13 @@
 import ReactCurvedText from "react-curved-text";
 const Mycomponent = () => {
   return (
-    <h4 className="heloocurve">
+    <h4 className="heloocurve container-fluid title-wraper">
       <ReactCurvedText
         width={1000}
         height={150}
-        cx={430}
-        cy={190}
-        rx={105}
+        cx={210}
+        cy={200}
+        rx={100}
         ry={170}
         startOffset={100}
         text="LEMLEM SCHOOL"
